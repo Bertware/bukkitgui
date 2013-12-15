1be3b6100809bc02edc3c51c9aa1d38fd9464474
Bertware <contact@bertware.net>
2013-12-15 17:23:49 +0100

    git setup finished

--- Translator/controls/A1Panel/Globals.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Text
-
-Namespace Owf.Controls
-    Friend Class A1PanelGlobals
-        Public Const A1Category As String = "A1"
-    End Class
-End Namespace
